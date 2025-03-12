44873616ebdcac57af8567a053a6ec3d14e15a76
nguyenthedat <dnguyenthe706@gmail.com>
2025-03-13 00:01:58 +0700

    thay doi moi

---
+++ src/buoi1/Exercise01.java
@@ -0,0 +1,10 @@
+package buoi1;
+
+public class Exercise01 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Computer Science");
+        System.out.println("Programming is fun");
+    }
+}
+
