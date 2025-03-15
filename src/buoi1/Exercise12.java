2c5927fe83378594d9c6762c24cb2389e5990fa2
nguyenthedat <dnguyenthe706@gmail.com>
2025-03-16 02:19:51 +0700

    test

--- src/buoi1/Exercise12.java
+++ src/buoi1/Exercise12.java
@@ -1,4 +1,4 @@
-public class Exercise12 {
+wpublic class Exercise12 {
     public static void main(String[] args) {
         System.out.println("" + (538 / 100) + ( 538 % 100) / 10 + (538 % 100) % 10);
     }
