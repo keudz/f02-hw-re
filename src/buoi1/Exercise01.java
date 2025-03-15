221f133eb749af8579e379d712d4200af5dde892
nguyenthedat <dnguyenthe706@gmail.com>
2025-03-16 01:21:32 +0700

    first commit

--- src/buoi1/Exercise01.java
+++ src/buoi1/Exercise01.java
@@ -5,6 +5,5 @@ public class Exercise01 {
         System.out.println("Welcome to Java");
         System.out.println("Welcome to Computer Science");
         System.out.println("Programming is fun");
-    }
 }
 
